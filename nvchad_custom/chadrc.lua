5275066da5396dcac30bd9704bacda7b8f525940
Yogesh Rathod <yogesh952003@gmail.com>
2024-01-27 14:41:01 +0530

    moved nvchad/lua/custom to dotfiles/custom

---
+++ nvchad_custom/chadrc.lua
@@ -0,0 +1,27 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "solarized_dark",
+  theme_toggle = { "solarized_dark", "gruvbox" },
+  transparency = true,
+  cmp = {
+    style = "default",
+  },
+  statusline = {
+    theme = "vscode_colored",
+  },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
