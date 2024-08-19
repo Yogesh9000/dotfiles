e1217fb611b3234ead0b967a603cfb7e884ab9b3
Yogesh Rathod <yogesh952003@gmail.com>
2024-08-19 22:11:43 +0530

    removed trailing whitespace indicatior and removed the 'jk' keymap and added the plantuml plugin

--- lazynvim/lua/config/keymaps.lua
+++ lazynvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 local del = vim.keymap.del
 
 map("n", ";", ":", { desc = "Commandline" })
-map("i", "jk", "<Esc>", { desc = "Normal mode" })
+-- map("i", "jk", "<Esc>", { desc = "Normal mode" })
 map("n", "<leader>bd", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
 
 -- terminal
---
+++ lazynvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+vim.opt.listchars = {
+  tab = "> ",
+  trail = " ",
+  nbsp = "+",
+}
