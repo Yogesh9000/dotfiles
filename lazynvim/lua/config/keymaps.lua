247e29803d36055e5dc2b9379ce9a880e37f2617
Yogesh Rathod <yogesh952003@gmail.com>
2024-12-19 19:51:40 +0530

    Add keymaps for closing and opening terminal in cwd

--- lazynvim/lua/config/keymaps.lua
+++ lazynvim/lua/config/keymaps.lua
@@ -6,3 +6,9 @@ map("n", ";", ":", { desc = "Commandline" })
 -- map("n", "<leader>bd", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
 
 map("i", "<C-BS>", "<C-W>", { desc = "delete previous word" })
+
+-- Snacks Terminal
+map("n", "<A-v>", function()
+  Snacks.terminal()
+end, { desc = "Terminal (cwd)" })
+map("t", "<A-v>", "<cmd>close<cr>", { desc = "Hide Terminal" })
