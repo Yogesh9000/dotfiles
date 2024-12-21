9f5ff95e7e64b3c84c6e677f576965e7d34efbff
Yogesh Rathod <yogesh952003@gmail.com>
2024-12-21 14:09:32 +0530

    Remove line importing telescope in lua/config/lazu.lua

--- lazynvim/lua/config/lazy.lua
+++ lazynvim/lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.editor.telescope" },
     -- import/override with your plugins
     { import = "plugins" },
   },
