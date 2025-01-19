e30baccf39e9d12857bd6843dd2a0689eca669d1
Yogesh Rathod <yogesh952003@gmail.com>
2025-01-19 22:04:48 +0530

    Fix broken keymapping for deleting entire word on C-BS

--- lazynvim/lua/config/keymaps.lua
+++ lazynvim/lua/config/keymaps.lua
@@ -5,7 +5,7 @@ map("n", ";", ":", { desc = "Commandline" })
 -- map("i", "jk", "<Esc>", { desc = "Normal mode" })
 -- map("n", "<leader>bd", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
 
-map("i", "<C-BS>", "<C-W>", { desc = "delete previous word" })
+map("i", "<C-H>", "<C-W>", { desc = "delete previous word" })
 
 -- Snacks Terminal
 map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
