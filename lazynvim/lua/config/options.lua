8b15d52766f05c00ae41e88d53d543d783d20023
Yogesh Rathod <yogesh952003@gmail.com>
2026-02-10 23:11:19 +0530

    Feat: set pwsh as shell on windows

--- lazynvim/lua/config/options.lua
+++ lazynvim/lua/config/options.lua
@@ -6,3 +6,7 @@ vim.opt.listchars = {
 vim.g.asmsyntax = "gas"
 vim.opt.cinoptions:append({ "l1", "g0" })
 vim.o.relativenumber = false
+
+if vim.fn.has("win32") then
+  vim.o.shell = "pwsh.exe -NoLogo"
+end
