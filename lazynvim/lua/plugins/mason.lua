2cd5309c3d482c2632c1d6b9f4ebf2d5564e942f
Yogesh Rathod <yogesh952003@gmail.com>
2026-03-21 19:35:48 +0530

    feat(lazynvim): Add roslyn.nvim for csharp development

--- lazynvim/lua/plugins/mason.lua
+++ lazynvim/lua/plugins/mason.lua
@@ -1,6 +1,10 @@
 return {
   "mason-org/mason.nvim",
   opts = {
+    registries = {
+      "github:mason-org/mason-registry",
+      "github:Crashdummyy/mason-registry",
+    },
     ensure_installed = {
       -- "html-lsp",
       -- "css-lsp",
